080dc3f3f42d92e2c678c8a18d49f0f323b2bc49
Vitalijus <L.Vitalijus@gmail.com>
2018-01-17 10:17:20 +0100

    Initial commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'zYypUJzbqClkLe2GvAviVl69I'
+CONSUMER_SECRET = 'eCog4JqmWmf9nuhDDOCpgpqzdTolQ0P2CIplUNLEpAHrTVNWeU'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
---
+++ user.py
@@ -0,0 +1,45 @@
+import json
+import oauth2
+from database import CursorFromConnectionFromPool
+from twitter_utils import consumer
+
+
+class User:
+    def __init__(self, screen_name, oauth_token, oauth_token_secret, id):
+        self.screen_name = screen_name
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
+        self.id = id
+
+    def __repr__(self):
+        return "<User {}>".format(self.screen_name)
+
+
+    def save_to_db(self):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute("INSERT INTO users (screen_name, oauth_token, oauth_token_secret) VALUES (%s, %s, %s)",
+                            (self.screen_name, self.oauth_token, self.oauth_token_secret))
+
+
+    @classmethod
+    def load_from_db_by_screen_name(cls, screen_name):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute("SELECT * FROM users WHERE screen_name = %s", (screen_name,))
+            user_data = cursor.fetchone()
+            if user_data:
+                return cls(screen_name=screen_name, oauth_token=user_data[2],
+                           oauth_token_secret=user_data[3], id=user_data[0])
+
+
+
+    def twitter_request(self, uri, verb='GET'):
+        authorized_token = oauth2.Token(self.oauth_token, self.oauth_token_secret)
+        authorized_client = oauth2.Client(consumer, authorized_token)
+
+        response, content = authorized_client.request(uri, verb)
+        if response.status != 200:
+            print("An error occured when seaching!")
+
+        return json.loads(content.decode('utf-8'))
+
+
